eec0407a0bae155495a89d2119771fcc21e7c053
Diyorbek Bekmurodov <bekmurodovdiyor25@gmail.com>
2023-02-22 15:28:38 +0500

    subject-crud-front-diyor

--- src/main/java/uz/pdp/spring_boot_security_web/adminController/AdminControllerUpSubject.java
+++ src/main/java/uz/pdp/spring_boot_security_web/adminController/AdminControllerUpSubject.java
@@ -1,12 +1,12 @@
 package uz.pdp.spring_boot_security_web.adminController;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import uz.pdp.spring_boot_security_web.entity.SubjectEntity;
 import uz.pdp.spring_boot_security_web.model.dto.SubjectRequestDTO;
+import uz.pdp.spring_boot_security_web.repository.SubjectRepository;
 import uz.pdp.spring_boot_security_web.service.SubjectService;
 
 import java.util.List;
@@ -16,8 +16,8 @@ import java.util.List;
 @RequestMapping("/adminSubject")
 public class AdminControllerUpSubject {
     private final SubjectService subjectService;
+    private final SubjectRepository subjectRepository;
 
-    @PreAuthorize("hasRole('ADMIN') and hasAuthority('ADD') or hasRole('SUPER_ADMIN')")
     @PostMapping("/addSubject")
     public String addSubject(@ModelAttribute SubjectRequestDTO subjectRequestDTO) {
         SubjectEntity add = subjectService.add(subjectRequestDTO);
@@ -27,7 +27,7 @@ public class AdminControllerUpSubject {
         return "redirect:/404";
     }
 
-    @PreAuthorize("hasRole('ADMIN') and hasAuthority('READ') or hasRole('SUPER_ADMIN')")
+
     @GetMapping("/subjects")
     public String getSubjectsList(Model model) {
         List<SubjectEntity> subjectEntities = subjectService.getList();
@@ -35,9 +35,8 @@ public class AdminControllerUpSubject {
         return "admin/subjectPageForAdmin";
     }
 
-    @PreAuthorize("hasRole('ADMIN') and hasAuthority('DELETE') or hasRole('SUPER_ADMIN')")
     @GetMapping("/deleteSubject/{id}")
-    public String getSubjectsList(@PathVariable("id") int id) {
+    public String getSubjectsList(@PathVariable int id) {
         boolean delete = subjectService.delete(id);
         if (delete){
             return "redirect:/adminSubject/subjects";
@@ -45,8 +44,6 @@ public class AdminControllerUpSubject {
         return "redirect:/404";
     }
 
-
-    @PreAuthorize("hasRole('ADMIN') and hasAuthority('GET') or hasRole('SUPER_ADMIN')")
     @GetMapping("/get/{id}")
     public String getById(@PathVariable("id") int id, Model model) {
         SubjectEntity byId = subjectService.getById(id);
@@ -55,5 +52,14 @@ public class AdminControllerUpSubject {
             return "redirect:/subject/list";
         }
         return "redirect:/404";
+    }
+
+    @PostMapping("/editSubject/{id}")
+    public String editSubject(
+            @PathVariable int id,
+            @ModelAttribute SubjectRequestDTO newName
+    ){
+        subjectService.editSubject(id,newName);
+        return "redirect:/adminSubject/subjects";
     }
 }
